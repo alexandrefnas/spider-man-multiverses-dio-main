985342bbcf54421719c9ebe874824c9523b8e03b
alexandrefnas <alexandrefnas@gmail.com>
2024-10-23 18:43:56 -0300

    Estilizando pagina principal, descrição dos filmes Tobey Maguire e estilizando botões

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -34,3 +34,18 @@ function selectCarouselItem(selectedButtonElement) {
   activeButtonElement.classList.remove('s-controller__button--active');
   selectedButtonElement.classList.add('s-controller__button--active');
 }
+
+function selectCarouselItemMenu(numer) {
+  const selectedItem = numer;
+  const carousel = document.querySelector('.s-cards-carousel');
+  const transform = carousel.style.transform;
+  const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
+  const rotateYDeg = -120 * (Number(selectedItem) - 1);
+  const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
+
+  carousel.style.transform = newTransform;
+
+  // const activeButtonElement = document.querySelector('.s-controller__button--active');
+  // activeButtonElement.classList.remove('s-controller__button--active');
+  // selectedButtonElement.classList.add('s-controller__button--active');
+}
